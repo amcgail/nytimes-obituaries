17ab6ba4fe00395d2b468fb51a6c764e2111a77c
Alec McGail <amcgail2@gmail.com>
2018-06-29 00:39:03 -0400

    comeon

--- _scripts/simple_coding_check.py
+++ _scripts/simple_coding_check.py
@@ -5,4 +5,4 @@ coder.loadPreviouslyCoded("codingAll", N=50, rand=False)
 
 for x in coder.obituaries:
     print(x['firstSentence'])
-    print(["[%s] %s: %s" % (y['term'], code, occ.codeToName[code]) for y in x['guess'] for code in y['occ']])
+    print(["[%s] %s: %s" % (y, code, occ.codeToName[code]) for y in x['guess'] for code in y['occ']])
