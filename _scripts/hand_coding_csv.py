af2284ddf1b1b8df01d5bab5f4a15d7e61a7a11b
Alec McGail <amcgail2@gmail.com>
2018-08-17 16:31:13 -0400

    handcoding

--- _scripts/hand_coding_csv.py
+++ _scripts/hand_coding_csv.py
@@ -1,11 +1,21 @@
+from os import path
 import occ
 import csv
+import os
 
-outfn = "hand_coding_4.csv"
-noutput = 100
+outfn = "hand_coding_6.csv"
+noutput = 300
 
 coder = occ.Coder()
-coder.loadPreviouslyCoded("codingAll", N=noutput, rand=True)
+coder.loadPreviouslyCoded("codingAll", N=noutput*2, rand=True)
+
+hand_coding_dir = path.join( path.dirname(__file__), "..", "hand_coding_originals" )
+
+old_ids = set()
+for partial_fn in os.listdir(hand_coding_dir):
+    fn = path.join(hand_coding_dir, partial_fn)
+    with open(fn) as f:
+        old_ids.update( int(x['id']) for x in csv.DictReader(f) )
 
 csv_rows = [
     [
@@ -15,9 +25,12 @@ csv_rows = [
         obit['fullBody']
     ]
     for obit in coder.obituaries
+    if int(obit['id']) not in old_ids
 ]
+print(len(csv_rows))
+csv_rows = csv_rows[:noutput]
 
-with open(outfn, 'w') as outf:
+with open(path.join(hand_coding_dir, outfn), 'w') as outf:
     csvw = csv.writer(outf)
     csvw.writerow(["id", "title", "firstSentence", "fullBody"])
     csvw.writerows(csv_rows)
